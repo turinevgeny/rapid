1304b40898ac589661f9a75fca0ffc0dadf819e6
ljaljushkin <ljaljushkin.nikolaj@gmail.com>
2013-04-04 21:44:09 +0400

    Sources moved to the folder 'src'

---
+++ src/Model.hpp
@@ -0,0 +1,28 @@
+#ifndef __MODEL_H
+#define __MODEL_H
+
+#include <opencv2/core/core.hpp>
+#include <list>
+
+class Model
+{
+private:
+	cv::Mat					T;					// model coordinate system origin in camera coords
+	cv::Mat					cornerPoints[8];	// corner points in model coords
+	double					scaleCoeff;			// for explicit projecting. Obsolete.
+	cv::Mat					cameraMatrix, distortionCoefficients;
+	std::list<cv::Mat>		controlPoints;		// list of control points
+	int						pointsPerEdge;
+public:
+					Model(const cv::Mat &T, const cv::Mat *cornerPoints, int pointsPerEdge, const cv::Mat &cameraMatrix, const cv::Mat &distortionCoefficients);
+					~Model();
+	cv::Mat			Outline(const cv::Mat &source);			// projects the model onto the image
+private:
+	cv::Point2d		Project(const cv::Mat &_3DPoint, const cv::Mat &rotationVector, const cv::Mat &translateVector);
+	cv::Point2d		Project(const cv::Mat &_3DPoint);
+	cv::Point2d		Project(const cv::Mat &_3DPoint, double scaleCoeff, const cv::Point2d &translateVector);
+	void			SetControlPoints();						// fills control points list with points evenly located on the edges
+	void            AddControlPointsFromTheEdge(int i, int j);
+};
+
+#endif
