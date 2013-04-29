9a4467be557475fd1158615f734eb5289c11dde9
turinevgeny <turinevgen@gmail.com>
2013-04-29 16:51:17 +0400

    New calibration data put

--- src/RAPIDTracker.hpp
+++ src/RAPIDTracker.hpp
@@ -4,19 +4,20 @@
 #include <opencv2/core/core.hpp>
 #include "Model.hpp"
 
+// Describes possible search directions among a row of pixels
 enum Direction{HORIZONTAL, VERTICAL, UPWARD_DIAGONAL, DOWNWARD_DIAGONAL};
 
 class RAPIDTracker
 {
 private:
-	Model model;
+	Model		model;
 	std::string videoFile;
-private:
-	cv::Mat	ExtractEdges(const cv::Mat &image) const;
-	double GetDisplacement(cv::Point2d controlPoint,cv::Point2d companionPoint,const cv::Mat &edges, cv::Point2d &foundPoint);
 public:
-	RAPIDTracker(const std::string videoFile, const Model &model);
-	Model ProcessFrame(const cv::Mat &frame);
+				RAPIDTracker(const std::string videoFile, const Model &model);
+	Model		ProcessFrame(const cv::Mat &frame);
+private:
+	cv::Mat		ExtractEdges(const cv::Mat &image) const;
+	double		GetDisplacement(cv::Point2d controlPoint, cv::Point2d companionPoint, const cv::Mat &edges, cv::Point2d &foundPoint);
 };
 
 #endif
