da103c4bf1f791adcc1cb7808234a0e1f9c1f3ca
Tyurin Evgeniy <turinevgen@gmail.com>
2013-08-24 15:13:38 +0400

    Initialize commit for RapidTesting.cpp

---
+++ src/RapidTesting.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+// Model traits and its handling methods
+#include "Model.hpp"
+// Algorithm wrapper
+#include "RAPIDTracker.hpp"
+
+using std::cout;
+using std::cerr;
+using std::endl;
+
+int main(int argn, char* argv[])
+{
+	return 0;
+}
