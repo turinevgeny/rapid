422822827353bcc2e3f19fd2c1ebdf3bed887ff1
Tyurin Evgeniy <turinevgen@gmail.com>
2013-11-16 20:33:37 +0400

    Added gtest 1.7.0 sources; added gtest compilation and linking with an example test file

---
+++ src/test.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+//#include <algorithm>
+
+#include <gtest/gtest.h>
+
+#include <opencv2/core/core.hpp>
+
+namespace Maths
+{
+
+int Square(int value)
+{
+    return value*value;
+}
+
+double CrossProduct(const cv::Mat& a, const cv::Mat& b)
+{
+	return a.dot(b);
+}
+
+}
+
+TEST(firstTest, SquareOf10)
+{
+	EXPECT_EQ(100, Maths::Square(10));
+}
+
+TEST(firstTest, SquareOf9)
+{
+	EXPECT_EQ(81, Maths::Square(9));
+}
+
+TEST(secondTest, CrossProductTesting)
+{
+	cv::Mat A = cv::Mat::zeros(1, 30, CV_32F);
+	cv::Mat B = cv::Mat::zeros(1, 30, CV_32F);
+
+	EXPECT_EQ(0.0, Maths::CrossProduct(A, B));
+}
